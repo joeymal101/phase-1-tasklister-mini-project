582d4e4498811fb81bf1699f929b1588c22fe576
Joell Malone <joellmalone000@gmail.com>
2023-08-11 01:16:41 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTask(e.target.new_task_description.value)
+    form.reset()
+ })
 });
+
+function buildTask(newTask) {
+  let p = document.createElement('p');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${newTask} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#new_task_description').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
